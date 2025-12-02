3ac45c35e42f2d5f6ebba9d8b478158835efe5dd
ArthurDaniel02 <arthurd4niel02@gmail.com>
2025-12-02 16:11:06 -0300

    settings

--- edutrack/edutrack/urls.py
+++ edutrack/edutrack/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter 
 from sistema.views import AlunoViewSet, ProfessorViewSet, TurmaViewSet, MatriculaViewSet, PresencaViewSet 
 from rest_framework.authtoken.views import obtain_auth_token
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 router = DefaultRouter()
 router.register(r'alunos', AlunoViewSet) 
 router.register(r'professores', ProfessorViewSet) 
@@ -14,4 +15,7 @@ urlpatterns = [
  path('admin/', admin.site.urls), 
  path('api/', include(router.urls)), 
  path('api/token/', obtain_auth_token),
+ path('api/schema/', SpectacularAPIView.as_view(), name='schema'), 
+ path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'), 
+ path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
